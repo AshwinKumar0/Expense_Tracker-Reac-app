6c4c384805ab834f8bfadd96c863cc9a0325b85c
mrkool <ak1380042@gmail.com>
2023-01-05 22:50:35 +0530

    app modified

--- src/Components/Expen/ExpenseDate/ExpenseDate.js
+++ src/Components/Expen/ExpenseDate/ExpenseDate.js
@@ -1,13 +1,15 @@
 import './ExpenseDate.css'
 import React from 'react';
+import classNames from 'classnames';
 
 function ExpenseDate(props){
-
+    const {className} = props;
+    const classes = classNames('date',className)
     const month = props.date.toLocaleString('en-US',{month:'long'});
     const year = props.date.getFullYear();
     const day = props.date.toLocaleString('en-US',{day:'2-digit'});;
     return(
-        <div className='date'>
+        <div className={classes}>
                 <div className='day'>{ day }</div>
                 <div className='month'>{ month }</div>
                 <div className='year'>{ year }</div>
--- src/Components/Expen/ExpenseItem/ExpenseItem.js
+++ src/Components/Expen/ExpenseItem/ExpenseItem.js
@@ -4,17 +4,19 @@ import Card from '../../UI/Card/Card';
 import React,{ useState } from 'react';
 
 function ExpenseItem(props){
+   
     const [title,setTitle] = useState(props.title); 
     const clickHandler = () =>{
            setTitle('New Title..')
     }
     return(
         <Card className='items'>
-            <ExpenseDate date={props.date}/>
+            <ExpenseDate className='date' date={props.date}/>
             <div className='item-desc'>
                 <h2>{title}</h2>
                 <div className='item-price'>{props.amount}</div>
             </div>
+            <input className='item-change' placeholder='Input Titile'  type="text"></input>
             <button className='item-price' onClick={clickHandler}>Change Title</button>
         </Card>
         
